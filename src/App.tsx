2e0ed898acc643020aa6f31604c994998eca5df0
Morné Louw <morne.louw@stepstone.com>
2023-02-22 22:41:58 +0200

    style: basic layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App">Hello world</div>;
+    return (
+        <div className="App flex-container">
+            <header id="header" className="flex-item">
+                Header goes here
+            </header>
+            <nav className="flex-item">Navigation here</nav>
+            <section id="hero-block" className="flex-item">
+                Here Block Here
+            </section>
+            <section id="brand-container" className="flex-item">
+                Tagline Block Here
+            </section>
+            <main id="shop-container" className="flex-item">
+                Shop here
+            </main>
+        </div>
+    );
 }
 
 export default App;
