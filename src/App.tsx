b67efce146d7a674d17f7d34503b26a5b6230cf8
Morné Louw <morne.louw@stepstone.com>
2023-02-28 20:38:27 +0200

    chore: app clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BooksList } from "./components/BooksList";
-
 import "./App.css";
 
 function App() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<BooksList />} />
-                    <Route path="/about" element={<>About Page</>} />
-                    {/* Any routes that are not defined our found*/}
-                    <Route path="*" element={<>404 Page Not Found</>} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+    return <div>App</div>;
 }
 
 export default App;
