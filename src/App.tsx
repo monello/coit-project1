87ff16d264fcfdfa34625d46c4f001a5a8ed906c
Morné Louw <morne.louw@stepstone.com>
2023-02-28 19:35:09 +0200

    feat: adding eacrt-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BooksList } from "./components/BooksList";
+
 import "./App.css";
 
 function App() {
     return (
         <div>
-            <BooksList />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<BooksList />} />
+                    <Route path="/about" element={<>About Page</>} />
+                    {/* Any routes that are not defined our found*/}
+                    <Route path="*" element={<>404 Page Not Found</>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
