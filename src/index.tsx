e3ff1f3140c4a2c2adac45308913fb88d4b883c8
Morné Louw <morne.louw@stepstone.com>
2023-02-28 21:05:49 +0200

    refactor: change approach as the react-router-dom tutorial is confusing

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 import "./index.css";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-]);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
---
+++ src/pages/CreateIssue/CreateIssue.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CreateIssue = () => {
+    return <div>CreateIssue</div>;
+};
